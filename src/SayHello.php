6544860f1b79eb904fc4efd7dcb9904458ba0e7a
wiladog <wiladog@gmail.com>
2018-03-07 15:52:29 +0800

    init

---
+++ src/SayHello.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Xgold; 
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World!';
+    }
+}
---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wiladog
+ * Date: 07/03/2018
+ * Time: 3:42 PM
+ */
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use Xgold\SayHello;
+
+echo SayHello::world();
